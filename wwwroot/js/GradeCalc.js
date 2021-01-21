8d16f84de0660359350abe00b5cf0d51a61a17c3
Ethan Hollis <e.hollis32@gmail.com>
2021-01-20 18:59:50 -0700

    Add project files.

---
+++ wwwroot/js/GradeCalc.js
@@ -0,0 +1,7 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    alert("Your grade would be: " + (.5 * document.getElementById("assignment").value + .1 * document.getElementById("group").value +
+    .1 * document.getElementById("quiz").value + .2 * document.getElementById("exam").value + .1 * document.getElementById("intex").value) +"%")
+    
+    });
